e950a8bfcaad1ac9d24bc877754306814ba8c734
chulanpro <chulanpro5@gmail.com>
2023-11-26 03:14:43 +0700

    fix: fix phone number

--- components/ui/mobile-menu.tsx
+++ components/ui/mobile-menu.tsx
@@ -68,7 +68,7 @@ export default function MobileMenu() {
               <a href="/" className="flex font-medium w-full text-gray-600 hover:text-gray-900 py-2 justify-center" onClick={() => setMobileNavOpen(false)}>About</a>
             </li>
             <li>
-              <a href="http://zalo.me/0968464724" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 w-full my-2" onClick={() => setMobileNavOpen(false)}>
+              <a href="tel:0968464724" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 w-full my-2" onClick={() => setMobileNavOpen(false)}>
                 <span>Contact</span>
                 <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                   <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fill="#999" fillRule="nonzero" />
