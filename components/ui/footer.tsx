8ef09df49d863063e333b7f7690ae8f90605e7cb
Hồ Khánh Duy <68040131+chulanpro5@users.noreply.github.com>
2023-11-27 00:02:54 +0700

    update content

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -93,7 +93,7 @@ export default function Footer() {
                 <div className="w-full">
                   <label className="block text-sm sr-only" htmlFor="newsletter">Email</label>
                   <div className="relative flex items-center max-w-xs">
-                    <input id="newsletter" type="email" className="form-input w-full text-gray-800 px-3 py-2 pr-12 text-sm" placeholder="Email" required />
+                    <input id="newsletter" type="email" className="form-input w-full text-gray-800 px-3 py-2 pr-12 text-sm" placeholder="Email hoặc Số điện thoại" required />
                     <button type="submit" className="absolute inset-0 left-auto" aria-label="Subscribe">
                       <span className="absolute inset-0 right-auto w-px -ml-px my-2 bg-gray-300" aria-hidden="true"></span>
                       <svg className="w-3 h-3 fill-current text-green-600 mx-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
